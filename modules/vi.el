20351b07f5192f4ba3898bcbf52bf7c941fd2a5a
Mikael Kolanowski <mikael.kolanowski@venor.se>
2021-12-18 11:19:06 +0100

    Add evil-collection

--- modules/vi.el
+++ modules/vi.el
@@ -1,14 +1,15 @@
 (use-package evil
   :init
-  (evil-mode)
   (setq evil-want-keybinding nil)
+  (evil-mode)
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
   :config
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
-(use-package evil-collection)
+(use-package evil-collection
+  :init (evil-collection-init))
 
 (use-package key-chord
   :init (key-chord-mode)
